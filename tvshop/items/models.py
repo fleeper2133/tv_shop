daba5d5e0d5785add53d7cbcb6ac317e2068922e
LAPTOP-4Q0DUPQ9\ROG <IshenkoNikita905@mail.ru>
2023-04-22 17:16:45 +0300

    create tables "Tv" and "Model"

--- tvshop/items/models.py
+++ tvshop/items/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Tv(models.Model):
+    title = models.CharField("Название", max_length=255)
+    type_tv = models.CharField("Тип", max_length=255)
+    model = models.ForeignKey('Model', null=True, on_delete=models.SET_NULL, verbose_name="Модель")
+    content = models.TextField("Описание")
+    value = models.IntegerField("Цена")
+    discount_value = models.IntegerField("Цена со скидкой", null=True)
+    diagonal = models.IntegerField("Диагональ")
+    resolution = models.CharField("Разрешение", max_length=50)
+    frequency = models.IntegerField("Частота")
+    sound_power = models.IntegerField("Мощность звука")
+    wi_fi = models.BooleanField("wi-fi")
+    smart_tv = models.BooleanField()
+    time_create = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField("Публикация", default=True)
+    photo = models.ImageField("Фото", upload_to="tvs/%Y/%m/%d/")
+
+    class Meta:
+        verbose_name = "Телевизор"
+        verbose_name_plural = "Телевизоры"
+        ordering = ['-time_create']
+
+
+class Model(models.Model):
+    title = models.CharField("Название", max_length=255)
+    slug = models.SlugField("URL")
+
+    class Meta:
+        verbose_name = "Модель телевизора"
+        verbose_name_plural = "Модели телевизоров"
