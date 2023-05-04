274c1ef93102eb299c6bdf8816979e747549ace1
LAPTOP-4Q0DUPQ9\ROG <IshenkoNikita905@mail.ru>
2023-05-05 01:21:46 +0300

    add template cart

--- tvshop/cart/views.py
+++ tvshop/cart/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import TemplateView
+from items.models import Tv
+
+
+class CartView(TemplateView):
+    template_name = 'cart/cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart = self.request.session.get('cart')
+
+        if cart:
+            items_cart = Tv.objects.filter(pk__in=cart)
+            #  Считаем общую цену
+            total = 0
+            for item in items_cart:
+                total += cart[str(item.pk)]['quantity'] * item.get_value()
+
+            context['items_cart'] = items_cart
+            context['quantity_cart'] = len(items_cart)
+            context['total_cart'] = total
+
+        context['title'] = "Корзина"
+
+        return context
 
 
 class AddInCart(View):
