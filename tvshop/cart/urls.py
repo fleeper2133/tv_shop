dc398f3183a9904092e1773bc9df601ae2c37247
fleeper2133 <NikitaIshenko905@gmail.com>
2023-05-28 00:42:03 +0300

    Add create order for authentication and not users, and add update and create accounts

--- tvshop/cart/urls.py
+++ tvshop/cart/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = 'cart'
 
 urlpatterns = [
-    path('', CartView.as_view(), name="view"),
+    path('', CartOrderView.as_view(), name="view"),
     path('add/<int:id_item>', AddInCart.as_view(), name="add"),
     path('remove/<int:id_item>', RemoveFromCart.as_view(), name="remove"),
     path('minus/<int:id_item>', RemoveOneItemFromCart.as_view(), name='minus')
